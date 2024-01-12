649ed05e31d70126b94490a4405bda58a0be3915
nish-acharya <nischitha.dn@engagedly.com>
2024-01-12 10:29:08 +0530

    First commit

---
+++ src/main/java/com/git_demo/pages/homePage.java
@@ -0,0 +1,9 @@
+package com.git_demo.pages;
+
+public class homePage {
+
+	public void homePage1()
+	{
+		System.out.println("hello");
+	}
+}
---
+++ src/test/java/com/git_demo/tests/HomeTest.java
@@ -0,0 +1,12 @@
+package com.git_demo.tests;
+
+import org.junit.Test;
+
+public class HomeTest {
+
+	
+	@Test
+	public void hometest() {
+		System.out.println("tes");
+	}
+}
